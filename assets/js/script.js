7cc1dbf40db3caa93b1509f929e5fe8d167ab951
m081779 <marcoprincipio@hotmail.com>
2017-09-28 22:19:56 -0400

    initial commit, pushing file structure and some working code

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+var query ='';
+var numResults ='';
+var startYear ='';
+var endYear ='';
+
+
+
+$('#search').on('click', function () {
+
+
+	query = $('#input1').val();
+	numResults = $('#input2').val();
+	startYear = $('#input3').val();
+	endYear = $('#input4').val();
+
+	if (startYear.length<8) {
+		startYear+="0101";
+		console.log(startYear);
+	}
+
+	if (endYear.length<8) {
+		endYear+="0101";
+		console.log(endYear);
+	}
+
+	var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+
+	queryURL += '?' + $.param({
+	  'api-key': "33c676fd7fd14e90a532f9698ab4dd4a",
+	  'q': query,
+	  'begin_date': startYear,
+	  'end_date': endYear
+	});
+
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	}).done(function (response) {
+		console.log(response.response.docs);
+		// for (var i = 0; i< response)
+	});
+});
+
+$('#clear').on('click', function () {
+	$('#results').empty();
+});
+
